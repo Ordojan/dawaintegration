6142a7a4e877134d46c3bfc8bd8867429452980a
Andrius <Andrius.or@gmail.com>
2014-06-03 01:39:54 +0200

    implemented getting addresses in communes, inserting them into houseunit table or updating existing rows with incremented doorcount

--- src/dawaintegration.py
+++ src/dawaintegration.py
@@ -37,22 +37,83 @@ def importCommuneInformation(session):
         name = element['navn']
         communes.append(Kommune(id, name))
 
-    [session.add(commune) for commune in communes]
+    session.add_all(communes)
     session.commit()
 
 def importAdressInformation(session):
     communes = session.query(Kommune).all()
 
-        def getAddressesInCommune(commune):
-            #http://dawa.aws.dk/adresser?kommunekode=0326
-            url = 'http://dawa.aws.dk/adresser'
-            payload = {'kommunekode': commune.id}
-            headers = {'content-type': 'Accept-Encoding: gzip, deflate'}
+    def insertHouseunit(address):
+        uuid = address['id']
+        communeID = address['kommune']['kode']
+        roadID = address['vejstykke']['kode']
+        houseID = address['husnr']
+        # equalno
+        x = address['adgangspunkt']['koordinater'][0]
+        y = address['adgangspunkt']['koordinater'][1]
+        doorcount = 1
+        sognenr = address['sogn']['kode']
+        zip = address['postnummer']['nr']
+        sognenavn = address['sogn']['navn']
+
+        houseunit = Houseunit()
+        houseunit.ADGANGSADRESSE_UUID = uuid
+        houseunit.KOMMUNEID = communeID
+        houseunit.ROADID = roadID
+        houseunit.HOUSEID = houseID
+        houseunit.X = x
+        houseunit.Y = y
+        houseunit.DOORCOUNT = doorcount
+        houseunit.SOGNENR = sognenr
+        houseunit.ZIP = zip
+        houseunit.SOGNENAVN = sognenavn
+
+        session.add(houseunit)
+        session.commit()
+
+    def getAddressesInCommune(commune):
+        url = 'http://dawa.aws.dk/adresser'
+        #parameters = {'kommunekode': commune.id}
+        headers = {'Accept-Encoding': 'gzip, deflate'}
+
+        response = requests.get(url, params={'kommunekode': '0430'}, headers=headers)
+        return response.json()
+
+    addressData = getAddressesInCommune(None)
+
+    id = addressData[0]["adgangsadresse"]['id']
+
+    result = session.query(Houseunit).filter_by(ADGANGSADRESSE_UUID = id).first()
+
+    address = addressData[0]["adgangsadresse"]
+
+    def incrementDoorCountForHouseunit(address):
+        result = session.query(Houseunit).filter_by(ADGANGSADRESSE_UUID = address['id']).first()
+        result.DOORCOUNT = result.DOORCOUNT + 1
+        session.commit()
+
+    if result is None:
+        insertHouseunit(address)
+    else:
+        incrementDoorCountForHouseunit(address)
+
+
+
+    def insertOrUpdateHouseunits(address):
+#check uuid if it exists if it does update else insert
+        session.query(Houseunit).filter_by(ADGANGSADRESSE_UUID = address['adgangsadresse']['id'])
+
+
+# is already in table?
+#no? insert
+#yes? update doorcount by one
+
+#sam area
 
 
 def main():
     session = loadSession()
-    importCommuneInformation(session)
+    #importCommuneInformation(session)
     importAdressInformation(session)
 
 if __name__ == "__main__":
